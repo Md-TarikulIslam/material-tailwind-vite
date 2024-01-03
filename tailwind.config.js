781b29fb5d514b800e9b2db71d1395ed4df77f08
MdTarikulIslam <nayon1820@gmail.com>
2024-01-03 14:22:23 +0600

    3rd jan, 2023

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
- 
+
 module.exports = withMT({
   content: [
     "./index.html",
@@ -8,10 +8,21 @@ module.exports = withMT({
     "/node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "primary-darker": "#cc5600",
+      "primary-dark": "#fe601f",
+      primary: "#f28020",
+      "primary-light": "#feba85",
+      "primary-lighter": "#fef2e9",
+      bs: "#eff3f7", //bg-secondary
+      bp: "#fafafa", //bg-primary
+      tp: "#212121", //text-primary
+      ts: "#757575", //text-secondary
+    },
     extend: {
-      fontFamily:{
-        'Open Sans':['Open Sans','sans-serif']
-      }
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
     },
   },
   plugins: [],
